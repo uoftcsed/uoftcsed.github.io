eae3843aba7f26dc863069e88763ea04e9ba2b29
Andrew Petersen <andrew.petersen@utoronto.ca>
2019-03-09 10:14:36 -0500

    Comped papers

--- _pubs/combined.php
+++ _pubs/combined.php
@@ -1,5 +1,5 @@
 <?php
-$_GET['bib']='bibs/Campbell.bib;bibs/Cheng.bib;bibs/Craig.bib;bibs/Easterbrook.bib;bibs/Engels.bib;bibs/Estrada.bib;bibs/Gries.bib;bibs/Grossman.bib;bibs/Harrington.bib;bibs/Heap.bib;bibs/Horton.bib;bibs/Liu.bib;bibs/Patitsas.bib;bibs/Petersen.bib;bibs/Pitt.bib;bibs/Reid.bib;bibs/Rosenbloom.bib;bibs/Smith.bib;bibs/Tafliovich.bib;bibs/Williams.bib;bibs/Zhang.bib;bibs/ZhangL.bib;bibs/Zingaro.bib';
+$_GET['bib']='bibs/Campbell.bib;bibs/Cheng.bib;bibs/Craig.bib;bibs/Easterbrook.bib;bibs/Engels.bib;bibs/Estrada.bib;bibs/Gries.bib;bibs/Grossman.bib;bibs/Harrington.bib;bibs/Heap.bib;bibs/Horton.bib;bibs/Liu.bib;bibs/Patitsas.bib;bibs/Petersen.bib;bibs/Pitt.bib;bibs/Reid.bib;bibs/Rosenbloom.bib;bibs/Sharmin.bib;bibs/Smith.bib;bibs/Tafliovich.bib;bibs/Williams.bib;bibs/Zhang.bib;bibs/ZhangL.bib;bibs/Zingaro.bib';
 $_GET['all']=1;
 include('bibtexbrowser.php');
 ?>
