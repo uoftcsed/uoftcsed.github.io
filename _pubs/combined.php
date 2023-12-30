e25fd086b7cea228fd55e06f6ef7f64d71c0a8ec
Andrew Petersen <andrew.petersen@utoronto.ca>
2023-12-29 22:19:23 -0600

    ITiCSE WGs

--- _pubs/combined.php
+++ _pubs/combined.php
@@ -1,5 +1,5 @@
 <?php
-$_GET['bib']='bibs/Campbell.bib;bibs/Cheng.bib;bibs/Craig.bib;bibs/Easterbrook.bib;bibs/Engels.bib;bibs/Estrada.bib;bibs/Gries.bib;bibs/Grossman.bib;bibs/Harrington.bib;bibs/Heap.bib;bibs/Horton.bib;bibs/Liu.bib;bibs/Liut.bib;bibs/Patitsas.bib;bibs/Petersen.bib;bibs/Pitt.bib;bibs/Reid.bib;bibs/Rosenbloom.bib;bibs/Sharmin.bib;bibs/Simion.bib;bibs/Smith.bib;bibs/Tafliovich.bib;bibs/Williams.bib;bibs/Zhang.bib;bibs/ZhangL.bib;bibs/Zingaro.bib;bibs/Kazemitabaar.bib';
+$_GET['bib']='bibs/Campbell.bib;bibs/Cheng.bib;bibs/Craig.bib;bibs/Easterbrook.bib;bibs/Engels.bib;bibs/Estrada.bib;bibs/Gries.bib;bibs/Grossman.bib;bibs/Harrington.bib;bibs/Heap.bib;bibs/Horton.bib;bibs/Liu.bib;bibs/Liut.bib;bibs/Patitsas.bib;bibs/Petersen.bib;bibs/Pitt.bib;bibs/Reid.bib;bibs/Rosenbloom.bib;bibs/Sharmin.bib;bibs/Simion.bib;bibs/Smith.bib;bibs/Tafliovich.bib;bibs/Williams.bib;bibs/Zhang.bib;bibs/ZhangL.bib;bibs/Zingaro.bib;bibs/Kazemitabaar.bib;bibs/Zavaleta.bib;bibs/Sibia.bib';
 $_GET['all']=1;
 include('bibtexbrowser.php');
 ?>
